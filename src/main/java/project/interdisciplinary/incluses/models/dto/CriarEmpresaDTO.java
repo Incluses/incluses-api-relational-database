6995a550dca34eb8e92fa8c423f6a2a0b3978ea3
EnricoPieroCosta <enrico.santos@germinare.org.br>
2024-10-10 08:17:48 -0300

    adding changes to dto

--- src/main/java/project/interdisciplinary/incluses/models/dto/CriarEmpresaDTO.java
+++ src/main/java/project/interdisciplinary/incluses/models/dto/CriarEmpresaDTO.java
@@ -1,20 +1,64 @@
 package project.interdisciplinary.incluses.models.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.br.CNPJ;
+
 import java.util.UUID;
 
 public class CriarEmpresaDTO {
+
+    @NotNull(message = "o cnpj não pode ser nulo")
+    @CNPJ(message = "o cnpj deve ser válido")
     private String cnpj;
+
+    @NotNull(message = "a razaoSocial não pode ser nula")
+    @Size(max = 100)
     private String razaoSocial;
+    @Size(max = 220)
     private String website;
+
+    @NotNull(message = "o nome não pode ser nulo")
+    @Size(max = 200 ,message = "O nome não pode passar de 50 caracteres")
     private String setor;
+
+    @NotNull(message = "a rua não pode ser nula")
+    @Size(max = 200, message = "a rua não pode passar de 200 caracteres")
     private String rua;
+
+    @NotNull(message = "o estado não pode ser nulo")
+    @Size(max = 100, message = "o estado não pode passar de 100 caracteres")
     private String estado;
+
+    @NotNull(message = "a cidade não pode ser nula")
+    @Size(max = 100, message = "a cidade não pode passar de 100 caracteres")
     private String cidade;
+
+    @NotNull(message = "o cep não pode ser nulo")
+    @Size(max = 8, min = 8, message = "o cep deve ter 8 caracteres")
     private String cep;
+
+    @NotNull(message = "O número não pode ser nulo")
+    @Min(value = 0, message = "o numero não pode conter valores negativos")
     private Integer numero;
+    @NotNull(message = "o nome não pode ser nulo")
+    @Size(max = 100, message = "o nome não pode passar de 100 caracteres")
     private String nome;
+    @NotNull(message = "o nome não pode ser nulo")
+    @Size(max = 50, message = "a senha não pode passar de 50 caracteres")
     private String senha;
+
+    @NotNull(message = "o email não pode ser nulo")
+    @Email(message = "o email deve ser válido")
+    @Size(max = 100, message = "o email não pode passar de 100 caracteres")
     private String email;
+
+    @NotNull(message = "o telefone não pode ser nulo")
+    @Size(min = 11, max = 11)
     private String telefone;
 
     // Getters e Setters
